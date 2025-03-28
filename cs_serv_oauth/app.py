220b83c2b0a70fc3d7e258c4e8cae41c67b2eb2f
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-03-28 02:05:13 -0500

    Integrando funcion oauth la cual utiliza basic Auth, para la generacion de token

--- cs_serv_oauth/app.py
+++ cs_serv_oauth/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+import jwt
+import datetime
+import uuid
 
 app = Flask(__name__)
 
+@app.route("/token", methods=["POST"])
+def generate_token():
+    auth = request.authorization
+    if not auth or auth.username != "admin" or auth.password != "123456":
+        return jsonify({"error": "Invalid credentials"}), 401
+    
+    return jsonify({"token": "token"})
 
 if __name__ == "__main__":
     app.run(port=5000)
