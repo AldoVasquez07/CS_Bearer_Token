160b8c30123b6862ad3fa6cbcf546878ee44a3f0
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-03-28 03:01:23 -0500

    Separando lógica en diferentes archivos

--- cs_serv_oauth/app.py
+++ cs_serv_oauth/app.py
@@ -4,31 +4,19 @@ import datetime
 import uuid
 import secrets
 
+from entity.models import User
+from security.secret_key import SecretKeyAuth
+
+
+SECRET_KEY = SecretKeyAuth.get_secret_key()
 
 app = Flask(__name__)
-
-
-def get_secret_key():
-    with open('../sec_workspace/SECRET_KEY.txt', 'r') as file:
-        return file.read().strip()
-
-
-def get_user(username, password):
-    with open("../bd_simulation/users.txt", "r") as users:
-        for user in users:
-            stored_username, stored_password = user.strip().split("|")
-            if stored_username == username and stored_password == password:
-                return True
-    return False
-
-
-SECRET_KEY = get_secret_key()
 
 
 @app.route('/token', methods=['POST'])
 def generate_token():
     auth = request.authorization
-    if not get_user(auth.username, auth.password):
+    if not User.get_user(auth.username, auth.password):
         return jsonify({'error': 'Invalid credentials'}), 401
     
     token = jwt.encode({
---
+++ cs_serv_oauth/entity/models.py
@@ -0,0 +1,8 @@
+class User:
+    def get_user(username, password):
+        with open('../bd_simulation/users.txt', 'r') as users:
+            for user in users:
+                stored_username, stored_password = user.strip().split('|')
+                if stored_username == username and stored_password == password:
+                    return True
+        return False
