12da4b67b86cf8fa30f4b3e87cc099c79adbd0ed
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-03-28 03:06:05 -0500

    Integrando logica básica para el back

--- cs_serv_back/app.py
+++ cs_serv_back/app.py
@@ -4,5 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/secure-data", methods=["GET"])
+def secure_data():
+    return jsonify({"message": "Access granted to secure data"})
+
+
 if __name__ == "__main__":
     app.run(port=5001)
