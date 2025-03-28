ec133b085e99f3695b06f8af69face1e3b121aba
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-03-28 02:38:01 -0500

    Integrando logica para verificar si el usuario está registrado

--- cs_serv_oauth/app.py
+++ cs_serv_oauth/app.py
@@ -9,26 +9,35 @@ app = Flask(__name__)
 
 
 def get_secret_key():
-    with open("../sec_workspace/SECRET_KEY.txt", "r") as file:
+    with open('../sec_workspace/SECRET_KEY.txt', 'r') as file:
         return file.read().strip()
+
+
+def get_user(username, password):
+    with open("../bd_simulation/users.txt", "r") as users:
+        for user in users:
+            stored_username, stored_password = user.strip().split("|")
+            if stored_username == username and stored_password == password:
+                return True
+    return False
 
 
 SECRET_KEY = get_secret_key()
 
 
-@app.route("/token", methods=["POST"])
+@app.route('/token', methods=['POST'])
 def generate_token():
     auth = request.authorization
-    if not auth or auth.username != "admin" or auth.password != "123456":
-        return jsonify({"error": "Invalid credentials"}), 401
+    if not get_user(auth.username, auth.password):
+        return jsonify({'error': 'Invalid credentials'}), 401
     
     token = jwt.encode({
-        "user": auth.username,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
-        "jti": str(uuid.uuid4())
-    }, SECRET_KEY, algorithm="HS256")
+        'user': auth.username,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+        'jti': str(uuid.uuid4())
+    }, SECRET_KEY, algorithm='HS256')
     
-    return jsonify({"token": token})
+    return jsonify({'token': token})
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=5000)
